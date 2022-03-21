65589173ebddd4f6e57873bf7cf5ec5695555fe2
Maxyz123 <max1391774324@gmail.com>
2022-03-21 14:20:33 +0000

    Updating MainPage and Reporting Page Route. Date: 21/03/2022

--- Assignment starting point/routes/index.js
+++ Assignment starting point/routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Image Browsing' });
 });
 
+//Main Page
+router.route('/mainPage')
+    .get(function (req,res){
+      res.render('mainPage',{title: "News Center"});
+    });
+
+//New Report
+router.route('/newReport')
+    .get(function (req,res){
+      res.render('newReport',{title: "News Upload"});
+    })
+
 module.exports = router;
