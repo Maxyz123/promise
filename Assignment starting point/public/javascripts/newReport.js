31fd703f90d6761f638dc61fdce6902a6d93ac23
Maxyz123 <max1391774324@gmail.com>
2022-04-12 14:23:02 +0100

    1. Implement Dynamic news Display 2. Implement onclick title or pictures to create chat room 3. configured url more reasonable

--- Assignment starting point/public/javascripts/newReport.js
+++ Assignment starting point/public/javascripts/newReport.js
@@ -1,26 +1,70 @@
 function sendAxiosQuery(url, data) {
+    if(url=='/newReport'){
+        axios.post(url, data)
+            .then((dataR) => {
+                var dataArr = JSON.parse(JSON.stringify(dataR.data));
+            })
+            .catch(function (response) {
+                alert(response.toJSON());
+            })
+    }
+
     axios.post(url, data)
         .then((dataR) => {
             var dataArr = JSON.parse(JSON.stringify(dataR.data));
             for (i in dataArr){
-                document.getElementById('results').innerHTML += dataArr[i]["author_name"];
-                document.getElementById('results').innerHTML += ", ";
-                document.getElementById('results').innerHTML += dataArr[i]["report_title"];
-                document.getElementById('results').innerHTML += ", ";
-                document.getElementById('results').innerHTML += dataArr[i]["report_text"];
-                document.getElementById('results').innerHTML += ", ";
-                document.getElementById('results').innerHTML += dataArr[i]["image_url"];
-                document.getElementById('results').innerHTML += ", ";
-                document.getElementById('results').innerHTML += dataArr[i]["date"];
-                document.getElementById('results').innerHTML += ", ";
-                document.getElementById('results').innerHTML += dataArr[i]["_id"];
-                document.getElementById('results').innerHTML += "<br>"
+                let title = dataArr[i]["author_name"]+" reported "+ dataArr[i]["report_title"] +
+                    " at " + dataArr[i]["date"];
+                let text = dataArr[i]["report_text"];
+                let imgUrl = dataArr[i]["image_url"]
+                let id = dataArr[i]["_id"];
+                let dashBoard = document.getElementById('dashBoard');
+
+                let paragraphTitle = document.createElement('h3');
+                let paragraphText = document.createElement('p');
+                let paragraphImg = document.createElement('img');
+                paragraphTitle.innerHTML = '<br>'+title+'<br>';
+                paragraphText.innerHTML = text+'<br>';
+                paragraphImg.src = imgUrl;
+                paragraphTitle.onclick=function (){
+                    var temp =document.createElement("form");
+                    temp.action='/index'
+                    temp.method='post'
+                    temp.style.display='none'
+                    var img = document.createElement("textarea");
+                    img.name="imageUrl";
+                    img.value=imgUrl;
+                    temp.appendChild(img)
+                    $("body").append(temp);
+                    temp.submit();
+                };
+                paragraphImg.onclick=function (){
+                    var temp =document.createElement("form");
+                    temp.action='/index'
+                    temp.method='post'
+                    temp.style.display='none'
+                    var img = document.createElement("textarea");
+                    img.name="imageUrl";
+                    img.value=imgUrl;
+                    temp.appendChild(img)
+                    $("body").append(temp);
+                    temp.submit();
+                };
+
+                dashBoard.appendChild(paragraphTitle);
+                dashBoard.appendChild(paragraphText);
+                dashBoard.appendChild(paragraphImg);
+                // scroll to the last element
+                dashBoard.scrollTop = dashBoard.scrollHeight;
+
             }
         })
         .catch(function (response) {
-            alert(response.toJSON());
+
         })
 }
+
+
 
 function onSubmit(url) {
     var formArray= $("form").serializeArray();
--- Assignment starting point/routes/index.js
+++ Assignment starting point/routes/index.js
@@ -5,8 +5,16 @@ var story = require('../controllers/stories');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Image Browsing' });
+  res.render('index', { title: 'Image Browsing', image: ''});
 });
+
+router.route('/index')
+    .get(function (req,res){
+        res.render('index',{title: 'Image Browsing', image: ''});
+    })
+    .post(function (req,res){
+        res.render('index',{title: 'Start your new Chat Room!', image: req.body.imageUrl});
+    });
 
 //Main Page
 router.route('/mainPage')
