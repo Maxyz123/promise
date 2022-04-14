11ff50da71c70b31863db7ff1466a45466549aef
Maxyz123 <max1391774324@gmail.com>
2022-04-14 07:55:19 +0100

    1. Fixed the duplicate submittion BUG. 2. Upload a new version of report document.

--- Assignment starting point/public/javascripts/newReport.js
+++ Assignment starting point/public/javascripts/newReport.js
@@ -3,65 +3,69 @@ function sendAxiosQuery(url, data) {
         axios.post(url, data)
             .then((dataR) => {
                 var dataArr = JSON.parse(JSON.stringify(dataR.data));
+                alert('New report: '+dataArr.report_title+' by Author '+ dataArr.author_name+'. Time: '+dataArr.date)
             })
             .catch(function (response) {
                 alert(response.toJSON());
             })
     }
 
-    axios.post(url, data)
-        .then((dataR) => {
-            var dataArr = JSON.parse(JSON.stringify(dataR.data));
-            for (i in dataArr){
-                let title = dataArr[i]["author_name"]+" reported "+ dataArr[i]["report_title"] +
-                    " at " + dataArr[i]["date"];
-                let text = dataArr[i]["report_text"];
-                let imgUrl = dataArr[i]["image_url"]
-                let id = dataArr[i]["_id"];
-                let dashBoard = document.getElementById('dashBoard');
+    else if (url=='/mainPage'){
+        axios.post(url, data)
+            .then((dataR) => {
+                var dataArr = JSON.parse(JSON.stringify(dataR.data));
+                for (i in dataArr){
+                    let title = dataArr[i]["author_name"]+" reported "+ dataArr[i]["report_title"] +
+                        " at " + dataArr[i]["date"];
+                    let text = dataArr[i]["report_text"];
+                    let imgUrl = dataArr[i]["image_url"]
+                    let id = dataArr[i]["_id"];
+                    let dashBoard = document.getElementById('dashBoard');
 
-                let paragraphTitle = document.createElement('h3');
-                let paragraphText = document.createElement('p');
-                let paragraphImg = document.createElement('img');
-                paragraphTitle.innerHTML = '<br>'+title+'<br>';
-                paragraphText.innerHTML = text+'<br>';
-                paragraphImg.src = imgUrl;
-                paragraphTitle.onclick=function (){
-                    var temp =document.createElement("form");
-                    temp.action='/index'
-                    temp.method='post'
-                    temp.style.display='none'
-                    var img = document.createElement("textarea");
-                    img.name="imageUrl";
-                    img.value=imgUrl;
-                    temp.appendChild(img)
-                    $("body").append(temp);
-                    temp.submit();
-                };
-                paragraphImg.onclick=function (){
-                    var temp =document.createElement("form");
-                    temp.action='/index'
-                    temp.method='post'
-                    temp.style.display='none'
-                    var img = document.createElement("textarea");
-                    img.name="imageUrl";
-                    img.value=imgUrl;
-                    temp.appendChild(img)
-                    $("body").append(temp);
-                    temp.submit();
-                };
+                    let paragraphTitle = document.createElement('h3');
+                    let paragraphText = document.createElement('p');
+                    let paragraphImg = document.createElement('img');
+                    paragraphTitle.innerHTML = '<br>'+title+'<br>';
+                    paragraphText.innerHTML = text+'<br>';
+                    paragraphImg.src = imgUrl;
+                    paragraphTitle.onclick=function (){
+                        var temp =document.createElement("form");
+                        temp.action='/index'
+                        temp.method='post'
+                        temp.style.display='none'
+                        var img = document.createElement("textarea");
+                        img.name="imageUrl";
+                        img.value=imgUrl;
+                        temp.appendChild(img)
+                        $("body").append(temp);
+                        temp.submit();
+                    };
+                    paragraphImg.onclick=function (){
+                        var temp =document.createElement("form");
+                        temp.action='/index'
+                        temp.method='post'
+                        temp.style.display='none'
+                        var img = document.createElement("textarea");
+                        img.name="imageUrl";
+                        img.value=imgUrl;
+                        temp.appendChild(img)
+                        $("body").append(temp);
+                        temp.submit();
+                    };
 
-                dashBoard.appendChild(paragraphTitle);
-                dashBoard.appendChild(paragraphText);
-                dashBoard.appendChild(paragraphImg);
-                // scroll to the last element
-                dashBoard.scrollTop = dashBoard.scrollHeight;
+                    dashBoard.appendChild(paragraphTitle);
+                    dashBoard.appendChild(paragraphText);
+                    dashBoard.appendChild(paragraphImg);
+                    // scroll to the last element
+                    dashBoard.scrollTop = dashBoard.scrollHeight;
 
-            }
-        })
-        .catch(function (response) {
+                }
+            })
+            .catch(function (response) {
 
-        })
+            })
+    }
+
 }
 
 
